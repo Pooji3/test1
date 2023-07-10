0ea6354ba5fca9aef5b09f061293e7f206cc645a
poojitha <116054129+Pooji3@users.noreply.github.com>
2023-07-10 16:07:28 +0530

    Add files via upload

---
+++ ContactUs.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const ContactUs = () => {
+  return (
+    <>
+    <div className='container mt-4'>
+        <h1>
+            FOR MORE DETAILS CONTACT
+        </h1>
+        <h3>
+            Dean : Rajesha S
+        </h3>
+        <h4>9XXXXXXXXX8</h4>
+    </div>
+    </>
+  )
+}
+
+export default ContactUs
