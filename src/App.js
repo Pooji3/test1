df9c59342924f6458b1a90a0a54cee4976e8ebc1
Jampala Poojitha <jampalapoojitha2003@gmail.com>
2023-07-12 17:32:50 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import "bootstrap/dist/css/bootstrap.min.css"
+
+import Home from './Components/Home';
+import About from './Components/About';
+import Contact from './Components/ContactUs';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Navbar from './Components/Navbar';
+import Courses from './Components/Courses';
+
+function App(){
+  return(
+    <>
+    {/* <h1 classname="bg-primary">React</h1>
+    <p>This is a paragraph</p> */}
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+      <Route path ="/" element={<Home/>}/>
+      <Route path ="/about" element={<About/>}/>
+      <Route path ="/courses" element={<Courses/>}/>
+      <Route path ="/contact" element={<Contact/>}/>
+      </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+export default App;
